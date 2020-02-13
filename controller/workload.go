070504785e9a0ac2bd35de8d7b88b31ab2244095
iceman739 <iceman86906@outlook.com>
2020-02-13 15:32:59 +0800

    add relicas

--- controller/workload.go
+++ controller/workload.go
@@ -1,17 +1,17 @@
 package controller
 
 import (
-	"github.com/hd-Li/types/apis/project.cattle.io/v3"
+	v3 "github.com/hd-Li/types/apis/project.cattle.io/v3"
 	appsv1beta2 "k8s.io/api/apps/v1beta2"
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	corev1 "k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/api/resource"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
 func NewDeployObject(component *v3.Component, app *v3.Application) appsv1beta2.Deployment {
 	ownerRef := GetOwnerRef(app)
 	containers, _ := getContainers(component)
-	deploy := appsv1beta2.Deployment {
+	deploy := appsv1beta2.Deployment{
 		ObjectMeta: metav1.ObjectMeta{
 			OwnerReferences: []metav1.OwnerReference{ownerRef},
 			Namespace:       app.Namespace,
@@ -20,16 +20,18 @@ func NewDeployObject(component *v3.Component, app *v3.Application) appsv1beta2.D
 			Annotations:     app.Annotations,
 		},
 		Spec: appsv1beta2.DeploymentSpec{
+			//add replicas
+			Replicas: &component.OptTraits.ManualScaler.Replicas,
 			Selector: &metav1.LabelSelector{
 				MatchLabels: map[string]string{
-					"app": app.Name + "-" + component.Name + "-" + "workload",
+					"app":     app.Name + "-" + component.Name + "-" + "workload",
 					"version": component.Version,
 				},
 			},
 			Template: corev1.PodTemplateSpec{
 				ObjectMeta: metav1.ObjectMeta{
 					Labels: map[string]string{
-						"app": app.Name + "-" + component.Name + "-" + "workload",
+						"app":     app.Name + "-" + component.Name + "-" + "workload",
 						"version": component.Version,
 					},
 				},
@@ -39,94 +41,94 @@ func NewDeployObject(component *v3.Component, app *v3.Application) appsv1beta2.D
 			},
 		},
 	}
-	
+
 	return deploy
 }
 
 func getContainers(component *v3.Component) ([]corev1.Container, error) {
 	var containers []corev1.Container
-	
+
 	for _, cc := range component.Containers {
 		ports := getContainerPorts(cc)
 		envs := getContainerEnvs(cc)
 		resources := getContainerResources(cc)
-		
-		container := corev1.Container {
-			Name: cc.Name,
-			Image: cc.Image,
-			Command: cc.Command,
-			Args: cc.Args,
-			Ports: ports,
-			Env: envs,
+
+		container := corev1.Container{
+			Name:      cc.Name,
+			Image:     cc.Image,
+			Command:   cc.Command,
+			Args:      cc.Args,
+			Ports:     ports,
+			Env:       envs,
 			Resources: resources,
 		}
-		
+
 		containers = append(containers, container)
 	}
-	
+
 	return containers, nil
 }
 
 func getContainerResources(cc v3.ComponentContainer) corev1.ResourceRequirements {
 	cpu := "500m"
 	mem := "200Mi"
-	
+
 	if cc.Resources.Cpu != "" {
 		cpu = cc.Resources.Cpu
 	}
-	
+
 	if cc.Resources.Memory != "" {
 		mem = cc.Resources.Memory
 	}
-	
+
 	resources := map[corev1.ResourceName]resource.Quantity{
-		corev1.ResourceCPU: resource.MustParse(cpu),
+		corev1.ResourceCPU:    resource.MustParse(cpu),
 		corev1.ResourceMemory: resource.MustParse(mem),
 	}
-	
-	rr := corev1.ResourceRequirements {
+
+	rr := corev1.ResourceRequirements{
 		Requests: resources,
-		Limits: resources,
+		Limits:   resources,
 	}
-	
+
 	return rr
 }
 
 func getContainerEnvs(cc v3.ComponentContainer) []corev1.EnvVar {
 	var envs []corev1.EnvVar
-	
+
 	for _, ccenv := range cc.Env {
-		env := corev1.EnvVar {
-			Name: ccenv.Name,
+		env := corev1.EnvVar{
+			Name:  ccenv.Name,
 			Value: ccenv.Value,
 		}
-		
+
 		envs = append(envs, env)
 	}
-	
+
 	return envs
 }
 
 func getContainerPorts(cc v3.ComponentContainer) []corev1.ContainerPort {
 	var ports []corev1.ContainerPort
-	
+
 	for _, ccp := range cc.Ports {
 		var proto corev1.Protocol
-		
-		if ccp.Protocol == "tcp" || ccp.Protocol == ""{
+
+		if ccp.Protocol == "tcp" || ccp.Protocol == "" {
 			proto = corev1.ProtocolTCP
-		}else {
+		} else {
 			proto = corev1.ProtocolUDP
 		}
-		
-		port := corev1.ContainerPort {
-			Name: ccp.Name,
+
+		port := corev1.ContainerPort{
+			Name:          ccp.Name,
 			ContainerPort: ccp.ContainerPort,
-			Protocol: proto,
+			Protocol:      proto,
 		}
-		
+
 		ports = append(ports, port)
 	}
-	
+
 	return ports
 }
